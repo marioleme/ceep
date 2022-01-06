e82d359ffd86a9bd6002d8c2b7d590c95f7e931b
Mario Oliveira <mario.oliveirag12@gmail.com>
2022-01-06 11:51:06 -0300

    Criando os componentes

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import CardNotas from "./CardNotas";
+
+class ListaDeNotas extends Component {
+    render() {
+        return (
+            <ul>
+                {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria) => {
+              return(
+                    <li>
+                        <div>{categoria}</div>
+                        <CardNotas />
+                    </li>
+                );
+                })};
+
+            </ul>
+        )
+    }
+}
+
+export default ListaDeNotas;
