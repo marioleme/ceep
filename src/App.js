591fc397570ba9662f74b0ec88b62acf291718d9
Mario Oliveira <mario.oliveirag12@gmail.com>
2022-01-06 16:14:23 -0300

    finalização do curso

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 import './assets/index.css';
+
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      notas: []
+    }
+  }
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novaArrayNotas =  [...this.state.notas,novaNota]
+    const novoEstado = {
+      notas: novaArrayNotas
+    }
+    this.setState(novoEstado)
+
+  }
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.state.notas} />
       </section>
     );
   }
