7db403317fdb2282536d419c720038000641cbe4
Mario Oliveira <mario.oliveirag12@gmail.com>
2022-01-06 14:12:01 -0300

    adicionado os estilos e dividindo os componentes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React  from 'react';
-import ListaDeNotas from './components/ListaDeNotas'
-import FormularioDeCadatro from './components/FormularioDeCadatro'
-import { Component } from 'react/cjs/react.development';
-
-class  App extends Component {
-    render(){
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+  render() {
     return (
-      <section>
-        <FormularioDeCadatro/>
-        <ListaDeNotas/>
+      <section className="conteudo">
+        <FormularioCadastro />
+        <ListaDeNotas />
       </section>
-    )
+    );
   }
 }
 
